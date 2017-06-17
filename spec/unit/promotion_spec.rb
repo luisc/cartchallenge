c19ae7edb283dba31b02ede49a43d8d0069192c1
Luis <luis@twentysidedstore.com>
2017-06-17 16:31:57 -0400

    reorganize tests and put in appropriate spec

--- spec/unit/promotion_spec.rb
+++ spec/unit/promotion_spec.rb
@@ -12,40 +12,5 @@ RSpec.describe Promotion do
     
     expect(promotion.qualifiers.length).to eq(2)
   end
-    
-  it "can add one adjustment at a time" do
-    
-    adjustment_1 = build(:adjustment)
-    adjustment_2 = build(:adjustment)
-    
-    order = build(:order)
-    order.add_adjustment(adjustment_1)
-    order.add_adjustment(adjustment_2)
-    
-    expect(order.adjustments.size).to eq(2)
-    
-  end
-  
-  it "can add an array of adjustments" do
-    adjustments = [build(:adjustment), build(:adjustment)]
-    order = build(:order)
-    
-    order.add_adjustments(adjustments)
-    
-    expect(order.adjustments.size).to eq(adjustments.size)
-  end
-  
-  it "can add promotions one at a time" do
-    
-    promotion_1 = build(:promotion)
-    promotion_2 = build(:promotion)
-    
-    order = build(:order)
-    
-    order.add_promotion(promotion_1)
-    order.add_promotion(promotion_2)
-    
-    expect(order.promotions.size).to eq(2)
-  end
   
 end
