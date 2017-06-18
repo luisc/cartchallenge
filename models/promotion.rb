ae61ee42b0f49961cf1cef5819e1ebb5931b6dee
Luis <luis@twentysidedstore.com>
2017-06-18 19:17:20 -0400

    add flag for repeat promotion application

--- models/promotion.rb
+++ models/promotion.rb
@@ -3,13 +3,20 @@ class Promotion
   attr_accessor :qualifiers
   attr_accessor :effects
   attr_accessor :results
+  attr_accessor :repeat_application
   
-  def initialize
-    self.qualifiers = []
+  def initialize(attrs = {})
+    self.qualifiers = {}
+    self.effects = {}
+    self.repeat_application = attrs[:repeat_application].nil? ? true : attrs[:repeat_application]
   end
   
   def add_qualifier(item, quantity)
-    self.qualifiers << { item: item, quanity: quantity }
+    self.qualifiers[item] = quantity
   end
-      
+  
+  def add_effect(label, amount)
+    self.effects[label] = amount
+  end
+  
 end
--- spec/unit/promotion_spec.rb
+++ spec/unit/promotion_spec.rb
@@ -13,4 +13,48 @@ RSpec.describe Promotion do
     expect(promotion.qualifiers.length).to eq(2)
   end
   
+  it "can have an arbitrary amount of effects" do
+    item_1 = build(:item)
+    item_2 = build(:item)
+    
+    promotion = Promotion.new
+    promotion.add_effect(item_1, BigDecimal("20"))
+    promotion.add_effect(item_2, BigDecimal("10"))
+    
+    expect(promotion.effects.length).to eq(2)
+  end
+  
+  it "defaults to applying for each match" do
+    promotion = Promotion.new
+    
+    expect(promotion.repeat_application).to eq true
+  end
+  
+  it "can set a flag to apply it for each match" do
+    promotion = Promotion.new(repeat_application: false)
+    
+    expect(promotion.repeat_application).to eq false
+  end
+  
+  context "applies a" do
+    
+    it "three for one deal" do
+      
+      item_1 = build(:item)
+      
+      promotion = Promotion.new
+      promotion.add_qualifier(item_1, 3)
+      promotion.add_effect("Buy 3, Get 1 Free", item.price)
+      
+      order = Order.new
+      order.add_line_item(item_1)
+      order.add_line_item(item_1)
+      order.add_line_item(item_1)
+      order.add_line_item(item_1)
+      
+      expect(true).to eq false
+    end
+    
+  end
+  
 end
